223d0fc2d534c6eec9d016f087723bf90afa0048
Shu Ning Bian <freespace@gmail.com>
2011-12-24 22:13:24 +1100

    new communication protocol to send/receive long strings and new sifontv1.c

--- examples/fonttest2/fonttest2.ino
+++ examples/fonttest2/fonttest2.ino
@@ -3,32 +3,70 @@
 
 #include "sifontv1.h"
 
-char string[] = "@freespace #arduino";
+char display_str[181];
 
+// returns length of string read, 0 if nothing was read
+int _readstring(char *buf, uint8_t bufsize) {
+  int cnt = 0;
+  
+  while(Serial.available() && cnt < bufsize-1) {
+    do {
+      int c = Serial.read();
+      switch(c) {
+        case '\r':
+        case '\n':
+        case '\t':
+          c = ' ';
+        default:
+          buf[cnt++] = c;
+      }
+    } while(Serial.available());
+    buf[cnt] = '\0'; 
+    Serial.println(cnt);
+    delay(100);
+  }
+  return cnt;
+}
+    
 void setup () {  
-  Serial.begin(9600);
+  Serial.begin(19200);
   delay(100);
   
   LedSign::Init(); 
   Font::SetGlyphs(Glyphs, Glyph_ASCII_Offset, Glyphs_Count);
+  Serial.println("ready");
 }
 
 int xoffset = 0;
-int x,w;
+boolean done;
+
 void loop() {
   LedSign::Clear();
-  x = 0;
-  for (int idx = 0; idx < strlen(string); ++idx) {
-    w=Font::Draw(string[idx], -xoffset+x,2);
-    if (w>0) x=x+w+1;
-    else if (w == 0) x+=2;
+  int x = 0;
+  int l = strlen(display_str);
+  if (l) {
+    for (int idx = 0; idx < l; ++idx) {
+      int w=Font::Draw(display_str[idx], -xoffset+x,2);
+      if (w>0) x=x+w+1;
+      else if (w == 0) x = x + 3;
+    }
+  } else {
+    Font::Draw('>', 0,2);
   }
 
-  if (xoffset) delay(200);
-  else delay(500);
+  if (xoffset) delay(80);
+  else delay(1000);
   
   xoffset+=1;
-  if (x<xoffset) xoffset = 0;
+  if (x<=xoffset) {
+    xoffset = 0;
+    if (_readstring(display_str, sizeof display_str)) {
+      Serial.print("display_str: ");
+      Serial.print(display_str);
+      Serial.print(" len: ");
+      Serial.println(strlen(display_str));
+    }
+  }
 }
 
 
